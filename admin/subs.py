d5c201a66106b660bda78e6d584bea1bbeacefff
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2012-12-19 04:43:45 +0100

    basic object list working

--- admin/subs.py
+++ admin/subs.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db.models import Model
 from django.core.paginator import Paginator
 from django.utils.importlib import import_module
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from composite.sub import Sub
 from composite.widget import Widget
@@ -13,16 +14,25 @@ class ObjectListWidget(Widget):
     template_name = 'admin/widgets/object_list.html'
 
     def get_context_data(self, page, request, *args, **kwargs):
-        ctx = super(ObjectList, self).get_context_data()
+        ctx = super(ObjectListWidget, self).get_context_data(page, request, *args, **kwargs)
         queryset = page.get_queryset(request)
-        objects = Paginator(queryset, 30)  # FIXME: hardcoded value
+        paginator = Paginator(queryset, 30)  # FIXME: hardcoded value
+        page = request.GET.get('page')
+        try:
+            objects = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            objects = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            objects = paginator.page(paginator.num_pages)
         ctx['objects'] = objects
         return ctx
 
 
 class ObjectList(OneColumn):
 
-    widgets = (ObjectListWidget,)
+    widgets = (ObjectListWidget(),)
 
     path = r'$'
 
@@ -48,30 +58,30 @@ class ModelAdmin(Sub):
     def __init__(self, model):
         super(ModelAdmin, self).__init__()
         self.model = model
-        self.register(ObjectList())
+        self.register(ObjectList)
 
     @property
-    def name(self):
-        return self.model._meta.module_name
-
-    @property
-    def app_name(self):
+    def app_label(self):
         return self.model._meta.app_label
 
     @property
+    def model_name(self):
+        return self.model._meta.module_name
+
+    @property
     def path(self):
-        return r'%s/%s' % (self.app_name, self.name)
+        return r'%s/%s' % (self.app_label, self.model_name)
 
 
 class Admin(Sub):
 
     app_name = 'newadmin'
-    name = 'newadmin'
+    namespace = 'newadmin'
     path = ''
 
-    def register(self, model, admin=None):
-        if admin:
-            admin = Admin(model)
+    def register(self, model, ModelAdminClass=None):
+        if ModelAdminClass:
+            admin = ModelAdminClass(model)
             super(Admin, self).register(admin)
         else:  # Use default ModelAdmin
             admin = ModelAdmin(model)
